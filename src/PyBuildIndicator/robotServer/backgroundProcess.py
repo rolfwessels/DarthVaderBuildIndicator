7d3e3827614db6a4aec08a02f6c5dbf42c1cfa29
Rolf Wessels <rolf.wessels@gmail.com>
2013-04-14 11:12:57 +0200

    Adding a background process for running the timed background process

--- src/PyBuildIndicator/robotServer/backgroundProcess.py
+++ src/PyBuildIndicator/robotServer/backgroundProcess.py
@@ -1,12 +1,47 @@
+import datetime
+from random import choice
+import threading
+import time
+
+
+class SleepingThread(threading.Thread):
+    def __init__(self, action, seconds=20000000):
+        self.sleepTime = seconds
+        self.action = action
+        self.stopped = False
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while not self.stopped:
+            time.sleep(self.sleepTime)
+            self.action()
 
 class PassiveManager():
     def __init__(self):
         self.CurrentPassive = Passive()
+        self.Timer = SleepingThread(self.ThreadTimerTick, 5)
+        self.Timer.start()
 
     def SetCurrentPassive(self, passive):
-        print "Setting new passive",passive
-        self.CurrentPassive = passive
-        pass
+        print "Setting new passive", passive
+        if isinstance(passive, Passive):
+            self.CurrentPassive = passive
+
+    def ThreadTimerTick(self):
+        if self.CurrentPassive.LastRun is None \
+            or (datetime.datetime.now() - self.CurrentPassive.LastRun).total_seconds() > self.CurrentPassive.Interval:
+            self.CurrentPassive.LastRun = datetime.datetime.now()
+            self.RunRandomSequence()
+        else:
+            seconds = (datetime.datetime.now() - self.CurrentPassive.LastRun).total_seconds()
+            print "The passive is ticking", seconds
+
+    def RunRandomSequence(self):
+        print "Running a Composition"
+        if len(self.CurrentPassive.Compositions) > 0:
+            print choice(self.CurrentPassive.Compositions)
+        else:
+            print "no items to choose from"
 
 
 class Sequences(object):
@@ -34,13 +69,18 @@ class Choreography(object):
 
 class Passive(object):
     def __init__(self):
-        self.Interval = 0
+        self.Initialize()
+
+    def __init__(self, **params):
+        self.Initialize()
+        self.__dict__.update(params)
+
+    def Initialize(self):
+        self.Interval = 5000
         self.StartTime = "06:00"
         self.SleepTime = "19:00"
+        self.LastRun = None
         self.Compositions = []
-
-    def __init__(self, ** params):
-        self.__dict__.update(params)
 
 
 # class SequencesPlaySound(Sequences):
