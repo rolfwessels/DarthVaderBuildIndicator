80cf33b685649196487d2913355b952902b7d93f
Rolf Wessels <rolf.wessels@gmail.com>
2016-06-13 22:44:12 +0200

    Adding some random text stuff.

---
+++ src/BuildIndicatron.Core/Chat/TellMeAQuotesContext.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BuildIndicatron.Core.Helpers;
+using BuildIndicatron.Core.Processes;
+using BuildIndicatron.Core.SimpleTextSplit;
+
+namespace BuildIndicatron.Core.Chat
+{
+    public class TellMeAQuotesContext : TextSplitterContextBase<TellMeAQuotesContext.Meta>, IWithHelpText
+    {
+        private readonly ITextToSpeech _textToSpeech;
+        private readonly IVoiceEnhancer _enhancer;
+
+        public TellMeAQuotesContext(ITextToSpeech textToSpeech, IVoiceEnhancer enhancer)
+        {
+            _textToSpeech = textToSpeech;
+            _enhancer = enhancer;
+        }
+
+        #region Implementation of IReposonseFlow
+
+        protected override void Apply(TextSplitter<Meta> textSplitter)
+        {
+            textSplitter.Map("(tell)(ANYTHING)(quote)(ANYTHING)");
+        }
+
+
+        protected override async Task Response(ChatContextHolder chatContextHolder, IMessageContext context, Meta server)
+        {
+            var oneLiner = RandomTextHelper.Quotes;
+            await context.Respond(oneLiner);
+            await _textToSpeech.Play(oneLiner, _enhancer);
+        }
+
+        #endregion
+
+        #region Implementation of IWithHelpText
+
+        public IEnumerable<HelpMessage> GetHelp()
+        {
+            yield return new HelpMessage { Call = "tell me a quote", Description = "Tells a quote." };
+        }
+
+        #endregion
+
+        #region Nested type: Meta
+
+        public class Meta
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ src/BuildIndicatron.Tests/Core/Chat/TellMeAQuotesContextTests.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BuildIndicatron.Tests.Core.Chat
+{
+    [TestFixture]
+    public class TellMeAQuotesContextTests : ChatBotTestsBase
+    {
+        #region Setup/Teardown
+
+        #endregion
+        
+        [Test]
+        public async Task Process_GivenSaySomething_ShouldTellMeAQuotesContext()
+        {
+            // arrange
+            Setup();
+
+            var messageContext = new MessageContext("tell me a quote");
+            // action
+            await _chatBot.Process(messageContext);
+            // assert
+            messageContext.LastMessages.Should().HaveCount(1);
+        }
+
+      
+    }
+}
