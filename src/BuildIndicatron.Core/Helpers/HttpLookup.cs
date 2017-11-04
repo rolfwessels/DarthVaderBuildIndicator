adade3cd2b876721ff0ce352b5138faef696842e
Rolf <rolf.wessels@gmail.com>
2017-11-04 16:16:13 +0200

    Update core to dotnet core.

--- src/BuildIndicatron.Core/Helpers/HttpLookup.cs
+++ src/BuildIndicatron.Core/Helpers/HttpLookup.cs
@@ -17,16 +17,29 @@ namespace BuildIndicatron.Core.Helpers
 
         #region Implementation of IHttpLookup
 
-        public async Task<IRestResponse> Download(string baseUrl, int timeout = 10000)
+        public Task<IRestResponse> Download(string baseUrl, int timeout = 10000)
         {
             var restClient = new RestClient(baseUrl) { Timeout = timeout };
-            if (!string.IsNullOrEmpty(_settings.GetDefaultProxy()))
+//            if (!string.IsNullOrEmpty(_settings.GetDefaultProxy()))
+//            {
+//                restClient.Proxy = new WebProxy(new Uri(_settings.GetDefaultProxy()));
+//            }
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse>();
+
+            restClient.ExecuteAsyncGet(new RestRequest(), (response, handle) =>
             {
-                restClient.Proxy = new WebProxy(new Uri(_settings.GetDefaultProxy()));
-            }
-            var restResponse = await restClient.ExecuteGetTaskAsync(new RestRequest());
+                if (response.ErrorException != null)
+                {
+                    taskCompletionSource.SetException(response.ErrorException);
+                }
+                else
+                {
+                    taskCompletionSource.SetResult(response);
+                }
+            },"GET");
+
             
-            return restResponse;
+            return taskCompletionSource.Task;
         }
 
         #endregion
