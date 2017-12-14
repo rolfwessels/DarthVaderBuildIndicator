edda9d1c96174d936f03f663fb35315b2a2c0f7a
Rolf Wessels <rolf.wessels@gmail.com>
2017-12-14 19:32:53 +0200

    Update init scripts. Adding rasberry pi 3 setup

--- var/AnsibleScripts/init.sh
+++ var/AnsibleScripts/init.sh
@@ -1,2 +1,3 @@
-ansible-playbook init-add-python -i hosts --limit 192.168.1.248 --ask-sudo-pass
-ansible-playbook init-wheel-user.yml -i hosts --limit 192.168.1.248 --ask-sudo-pass
+ssh-copy-id pi@192.168.1.102 && \
+ansible-playbook init-add-python.yml -i hosts --limit 192.168.1.102 --ask-sudo-pass && \
+ansible-playbook init-wheel-user.yml -i hosts --limit 192.168.1.102 --ask-sudo-pass
